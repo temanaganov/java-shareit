f32709d3c5e0254cf7e049e19861718fa651a877
Artem Naganov <artemnaganov10@gmail.com>
2023-03-02 15:23:53 +0300

    fix: checkstyle

--- gateway/src/main/java/ru/practicum/shareit/booking/CheckDateValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/CheckDateValidator.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 
 public class CheckDateValidator implements ConstraintValidator<StartBeforeEndDateValid, CreateBookingDto> {
     @Override
-    public void initialize(StartBeforeEndDateValid constraintAnnotation) {}
+    public void initialize(StartBeforeEndDateValid constraintAnnotation) {
+    }
 
     @Override
     public boolean isValid(CreateBookingDto bookingDto, ConstraintValidatorContext constraintValidatorContext) {
