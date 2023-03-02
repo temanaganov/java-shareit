9d28365d7ed2527874620907e8f631e4e9f5abad
Artem Naganov <artemnaganov10@gmail.com>
2023-03-02 20:11:03 +0300

    fix: правки

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/CreateBookingDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/CreateBookingDto.java
@@ -18,11 +18,9 @@ public class CreateBookingDto {
 	@NotNull(message = "ItemId is required")
 	Long itemId;
 
-	@NotNull(message = "Start is required")
 	@FutureOrPresent(message = "Start must be in future or present")
 	LocalDateTime start;
 
-	@NotNull(message = "End is required")
 	@Future(message = "End must be in future")
 	LocalDateTime end;
 }
